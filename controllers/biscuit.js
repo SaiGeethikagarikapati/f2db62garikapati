b357ab46ea6db3696d295a3f8d97ba62f213a95b
SaiGeethikagarikapati <108358561+SaiGeethikagarikapati@users.noreply.github.com>
2022-11-16 14:16:04 -0600

    Done all

--- controllers/biscuit.js
+++ controllers/biscuit.js
@@ -1,6 +1,6 @@
 var Biscuit = require('../models/biscuit'); 
  
-// List of all Costumes 
+// List of all biscuits 
 exports.biscuit_list = async function(req, res) { 
     try{ 
         theBiscuits = await Biscuit.find(); 
@@ -13,8 +13,15 @@ exports.biscuit_list = async function(req, res) {
 }; 
  
 // for a specific biscuit. 
-exports.biscuit_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: biscuit detail: ' + req.params.id); 
+exports.biscuit_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Biscuit.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
 
 
@@ -24,7 +31,7 @@ exports.biscuit_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    // {"biscuit_type":"goat", "cost":12, "size":"large"} 
     document.brandName = req.body.brandName; 
     document.price = req.body.price; 
     document.flavour = req.body.flavour; 
@@ -39,13 +46,37 @@ exports.biscuit_create_post = async function(req, res) {
 };
  
 // Handle biscuit delete form on DELETE. 
-exports.biscuit_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: biscuit delete DELETE ' + req.params.id); 
+exports.biscuit_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Biscuit.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 }; 
  
 // Handle biscuit update form on PUT. 
-exports.biscuit_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: biscuit update PUT' + req.params.id); 
+exports.biscuit_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Biscuit.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.biscuit_type)  
+               toUpdate.biscuit_type = req.body.biscuit_type; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
 
 exports.biscuit_view_all_Page = async function(req, res) { 
@@ -57,4 +88,53 @@ exports.biscuit_view_all_Page = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
+}; 
+
+exports.biscuit_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Biscuit.findById( req.query.id) 
+        res.render('biscuitdetail',  
+{ title: 'Biscuit Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+exports.biscuit_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('biscuitcreate', { title: 'Biscuit Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+exports.biscuit_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Biscuit.findById(req.query.id) 
+        res.render('biscuitupdate', { title: 'Biscuit Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
+exports.biscuit_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Biscuit.findById(req.query.id) 
+        res.render('biscuitdelete', { title: 'Biscuit Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
 }; 
--- routes/biscuits.js
+++ routes/biscuits.js
@@ -4,4 +4,8 @@ var router = express.Router();
  
 /* GET costumes */ 
 router.get('/', biscuit_controlers.biscuit_view_all_Page ); 
+router.get('/detail', biscuit_controlers.biscuit_view_one_Page); 
+router.get('/create', biscuit_controlers.biscuit_create_Page); 
+router.get('/update', biscuit_controlers.biscuit_update_Page); 
+router.get('/delete', biscuit_controlers.biscuit_delete_Page); 
 module.exports = router; 
